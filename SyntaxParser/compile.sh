c8d3e351efb3652bc233a43e128f164574621059
Kseniya <kitnoelka@gmail.com>
2017-12-21 08:59:29 +0300

    cleanup

--- SyntaxParser/compile.sh
+++
@@ -1,5 +0,0 @@
-yacc --verbose --debug -d syntaxer.y -o syntaxParser.cc
-lex lexer.lex 
-gcc -c lex.yy.c -o lex.yy.o -ll
-g++ lex.yy.o syntaxParser.cc -o example
-
--- SyntaxParser/compileC.sh
+++
@@ -1,3 +0,0 @@
-yacc -d syntaxer.y
-lex lexer.lex
-cc lex.yy.c y.tab.c -o testC
