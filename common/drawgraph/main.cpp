21f2346678b583df1d6f91ee2549eae2ac6049a0
ksenull <kitnoelka@gmail.com>
2018-06-10 03:00:36 +0300

    add pictures

--- common/drawgraph/main.cpp
+++ common/drawgraph/main.cpp
@@ -3,12 +3,12 @@
 
 #include "GraphV.h"
 
-static const std::string PathPrefix = "/home/ksenull/workspace/test/minijavac_17/";
+static const std::string PathPrefix = "/home/ksenull/workspace/test/minijavac_17/ir/";
 
 int main() {
     ast::visualize::Graph graph{};
-    graph.readFromFile(PathPrefix + "ir/graph.dot");
-    graph.savePng(PathPrefix + "common/drawgraph/graph.png");
+    graph.readFromFile(PathPrefix + "graph.dot");
+    graph.savePng(PathPrefix + "graph.png");
     graph.release();
     return 0;
 }
